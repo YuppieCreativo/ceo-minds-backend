c4f25adbb017d429726b527aadbbc9221498e887
lobobot1 <kevincolmenaresm@gmail.com>
2025-03-11 18:38:21 -0400

    [v0.0.1]: Add Backend_url env

--- src/index.ts
+++ src/index.ts
@@ -6,17 +6,18 @@ import cors from "cors";
 import { connectDB } from "./db";
 import { config } from "./config";
 import routes from "./routes/index";
+import cron from "node-cron";
 
 connectDB();
 
 const app: Application = express();
 
 app.use(
-	cors({
-		credentials: true,
-		methods: ["GET", "POST", "PUT"],
-		optionsSuccessStatus: 200,
-	}),
+  cors({
+    credentials: true,
+    methods: ["GET", "POST", "PUT"],
+    optionsSuccessStatus: 200,
+  })
 );
 
 app.use(compression());
@@ -26,5 +27,11 @@ app.use(bodyParser.json());
 app.use("/", routes);
 
 app.listen(config.PORT, () => {
-	console.log(`server running on http://localhost:${config.PORT}`);
+  console.log(`server running on http://localhost:${config.PORT}`);
+});
+
+cron.schedule("*/13 * * * *", async () => {
+  console.log("running a task every 13 minute");
+
+  await fetch(`${config.BACKEND_URL ?? ""}/api/user`);
 });
