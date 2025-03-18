a811f700055c6130cf8d2dc46da814a3fadfbbc6
lobobot1 <kevincolmenaresm@gmail.com>
2025-03-18 15:54:25 -0400

    [v0.0.1]: Fix subject princing email

--- src/controllers/emails.controller.ts
+++ src/controllers/emails.controller.ts
@@ -213,11 +213,10 @@ class EmailController {
   }
 
   async sendPricing({ email }: { email: string }) {
-
     const { error } = await resend.emails.send({
       to: [email],
       from: "info@ceominds.net",
-      subject: "Pricing",
+      subject: "Hola CEO Minder",
       html: `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
             <html dir="ltr" lang="en">
             <head>
