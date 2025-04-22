4742610a00743508b5c254b45f399ded836a8a75
lobobot1 <kevincolmenaresm@gmail.com>
2025-04-22 18:12:14 -0400

    [v1.1.1]: Email | attendance email

---
+++ emails/maginCarmona.tsx
@@ -0,0 +1,32 @@
+import {
+  Body,
+  Container,
+  Head,
+  Html,
+  Img,
+  Tailwind,
+} from "@react-email/components";
+import * as React from "react";
+
+const PricingEmail = () => {
+  return (
+    <Html>
+      <Head />
+      <Tailwind>
+        <Body className="py-10">
+          <Container>
+            <Img
+              src={
+                "https://yuppie-bucket.s3.us-east-1.amazonaws.com/ceominds/emails/magin+carmona.png"
+              }
+              className="rounded-lg object-center max-w-[95%] max-h-[95%] mx-auto"
+              alt="CEOMinds Attendance"
+            />
+          </Container>
+        </Body>
+      </Tailwind>
+    </Html>
+  );
+};
+
+export default PricingEmail;
